3326d4aa3a494fff3d1f63009733a44b6d247de5
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-02-20 08:44:08 +0100

    HelloWorld in Camel

---
+++ Camel-aplication/src/main/java/example1/HelloWorldRoute.java
@@ -0,0 +1,17 @@
+package example1;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class HelloWorldRoute extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world in cammel");	
+		
+	}
+
+	
+	
+}
