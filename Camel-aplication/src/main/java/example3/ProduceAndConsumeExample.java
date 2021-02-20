02fb8a8db68b79331f9ca866ed899acff6f68381
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-02-20 10:23:50 +0100

    Add processor to project

--- Camel-aplication/src/main/java/example3/ProduceAndConsumeExample.java
+++ Camel-aplication/src/main/java/example3/ProduceAndConsumeExample.java
@@ -2,10 +2,12 @@ package example3;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.ConsumerTemplate;
+import org.apache.camel.Exchange;
 import org.apache.camel.Message;
+import org.apache.camel.Processor;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.DefaultCamelContext;import org.apache.camel.support.ProcessorEndpoint;
 
 public class ProduceAndConsumeExample {
 
@@ -23,6 +25,16 @@ public class ProduceAndConsumeExample {
 				
 				
 				from("direct:start")
+				.process(new Processor() {
+					
+					public void process(Exchange exchange) throws Exception {
+						// TODO Auto-generated method stub
+						
+						System.out.println("I am the processor...");
+						
+						
+					}
+				})
 				.to("seda:end");
 				
 				
